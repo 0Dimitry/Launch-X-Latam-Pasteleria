fe135d6c58ac56cd0dbed910c28793d6c9488018
0Dimitry <santiagocaballero279@gmail.com>
2023-11-23 08:31:18 -0500

    new

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ Abrir.addEventListener("click", () => {
     nav.classList.add("visible");
 })
 
+Abrir.addEventListener("scroll", function(event){
+    event.preventDefault();
+    Abrir.scrollTop=0;
+}, false);
+
 Cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
